2ebafac7060b0a1b7e47b6841bbc0feb40811648
Tyrion <joega068472@gmail.com>
2023-05-19 00:04:01 +0800

    adjust password rule

--- src/common/method.js
+++ src/common/method.js
@@ -1,7 +1,7 @@
 import { toast } from 'react-toastify';
 export const toastType = Object.freeze({ info: 0, success: 1, warning: 2, error: 3 });
 const emailRule = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/;
-const passwordRule = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*_].{8,}$/;
+const passwordRule = /^(?=.*[A-Za-z])(?=.*\d)(?=.*\S).{8,}$/;
 export const showToast = (type, message) => {
     switch (type) {
         case toastType.info:
@@ -17,6 +17,7 @@ export const showToast = (type, message) => {
             toast.error(message);
             break;
         default:
+            toast(message);
             break;
     }
 
