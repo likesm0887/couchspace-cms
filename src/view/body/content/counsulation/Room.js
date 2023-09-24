5bb66e1563eab40a85bd62d2ed1bc3606514eea8
Tyrion <joega068472@gmail.com>
2023-09-25 02:54:36 +0800

    add camera/mic control for video

--- src/view/body/content/counsulation/Room.js
+++ src/view/body/content/counsulation/Room.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Participant from "./Participant";
 import "./counseling.css"
+import { Button, Image } from "antd";
+
 const Room = ({ roomName, room, handleLogout }) => {
   const [participants, setParticipants] = useState([]);
+  const [showCamera, setShowCamera] = useState(true);
+  const [showMic, setShowMic] = useState(true);
 
   useEffect(() => {
     const participantConnected = (participant) => {
@@ -28,10 +32,37 @@ const Room = ({ roomName, room, handleLogout }) => {
   const remoteParticipants = participants.map((participant) => (
     <Participant key={participant.sid} participant={participant} />
   ));
+  const onClickCamera = () => {
+    if (showCamera) {
+      room.localParticipant.videoTracks.forEach(publication => {
+        publication.track.disable();
+      });
+
+    }
+    else {
+      room.localParticipant.videoTracks.forEach(publication => {
+        publication.track.enable();
+      });
+    }
+    setShowCamera(!showCamera);
+  }
+  const onClickMic = () => {
+    if (showMic) {
+      room.localParticipant.audioTracks.forEach(publication => {
+        publication.track.disable();
+      });
+    }
+    else {
+      room.localParticipant.audioTracks.forEach(publication => {
+        publication.track.enable();
+      });
+
+    }
+    setShowMic(!showMic);
+  }
 
   return (
     <div className="room">
-
       <div className={"Screen"}>
         <div className={"local-participant"}>
           {room ? (
@@ -46,17 +77,11 @@ const Room = ({ roomName, room, handleLogout }) => {
         <div className={"remote-participants"}>
           {remoteParticipants}
         </div>
-
-
-        {/* <div>
-        Remote participants:{' '}
-        {JSON.stringify(remoteParticipants.map(v => v.identity))}
-      </div>
-      <h3>Remote Participants</h3> */}
-
       </div>
       <div className={"stopAndClose"}>
-        <p className={"stopWatch"}>00:00:10</p>
+        <div className={"stopWatch"}>00:00:10</div>
+        <button style={{ marginLeft: 20 }} onClick={onClickCamera}> <img style={{ height: 30, width: 40, verticalAlign: 'middle' }} src={showCamera ? require("../../../img/content/camera_enabled.png") : require("../../../img/content/camera_disabled.png")} alt="myCamera" />{"鏡頭"} </button>
+        <button style={{ marginLeft: 20 }} onClick={onClickMic}> <img style={{ height: 40, width: 25, verticalAlign: 'middle' }} src={showMic ? require("../../../img/content/mic_enabled.png") : require("../../../img/content/mic_disabled.png")} alt="myMic" /> {"麥克風"}</button>
         <button onClick={handleLogout} className={"stop"}>結束諮詢</button>
       </div>
     </div>
