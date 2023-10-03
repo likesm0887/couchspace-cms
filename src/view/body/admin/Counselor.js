42fad034ed5e44dfa06ed1ebf4ed012c802812de
DESKTOP-UO9P3A3\User <likesm0887@gmail.com>
2023-10-04 02:31:32 +0800

    upgrade antd

--- src/view/body/admin/Counselor.js
+++ src/view/body/admin/Counselor.js
@@ -1,51 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { Drawer, Button, Table, Statistic, Switch } from "antd";
+import {
+  Drawer,
+  Button,
+  Table,
+  Statistic,
+  Switch,
+  Tag,
+  Modal,
+  Menu,
+} from "antd";
+import { Layout, theme, Descriptions,Badge} from "antd";
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+
+  UserOutlined,
+} from "@ant-design/icons";
+
 import { counselorService } from "../../../service/ServicePool";
 import CountUp from "react-countup";
-import { async } from "@firebase/util";
-const DrawerForm = ({ id, visible, onClose, record,callback }) => {
+import img_account from "../../img/login/account.svg";
+const DrawerForm = ({ id, visible, onClose, record, callback }) => {
   // Define state for the form fields
-  const [name, setName] = useState(record?.name || "");
-  const [nickname, setNickname] = useState(record?.nickname || "");
-  const [photo, setPhoto] = useState(record?.photo || "");
-  const [account, setAccount] = useState(record?.account || "");
-  const [birthdate, setBirthdate] = useState(record?.birthdate || "");
-  const [membership, setMembership] = useState(record?.membership || "");
+
   const [verify, setVerify] = useState(false);
   // Clear form fields
-  const clearFields = () => {
-    setName("");
-    setNickname("");
-    setPhoto("");
-    setAccount("");
-    setBirthdate("");
-    setMembership("");
-  };
+
   const changeVerify = (e) => {
-   
     setVerify(e);
-    console.log(e)
+    console.log(e);
   };
 
   useEffect(() => {
-    console.log("JIJI")
+    console.log("JIJI");
     const getCounselorVerify = async () => {
       const isVerify = await counselorService.getCounselorVerify(id);
-      console.log(isVerify)
+      console.log(isVerify);
       setVerify(isVerify);
     };
     getCounselorVerify();
     console.log("HIHIH");
   }, [id]);
 
-
   // Handle form submission
   const handleSubmit = async () => {
-    console.log(verify)
+    console.log(verify);
     await counselorService.setCounselorVerify(id, verify);
     onClose();
     callback();
-    clearFields();
   };
 
   return (
@@ -63,9 +65,8 @@ const DrawerForm = ({ id, visible, onClose, record,callback }) => {
   );
 };
 
-
 const Counselor = () => {
-  
+  const { Header, Content, Footer, Sider } = Layout;
   const formatter = (value) => <CountUp end={value} separator="," />;
   const [visible, setVisible] = useState(false);
   const [record, setRecord] = useState(null);
@@ -74,10 +75,11 @@ const Counselor = () => {
   const [permiun, setPermiun] = useState(0);
   const [active, setActive] = useState(0);
   const [currentSelectCounselorId, setCurrentSelectCounselorId] = useState("");
-  
-
-
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   const columns = [
     {
       title: "Action",
@@ -89,9 +91,19 @@ const Counselor = () => {
       ),
     },
     {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
       title: "Name",
       dataIndex: "name",
       key: "name",
+      render: (text, record) => (
+        <a style={{ color: "#1677FF" }} onClick={() => setIsModalOpen(true)}>
+          {text}
+        </a>
+      ),
     },
     {
       title: "Nickname",
@@ -99,11 +111,37 @@ const Counselor = () => {
       key: "nickname",
     },
     {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+
+    {
       title: "Photo",
       dataIndex: "photo",
       key: "photo",
       render: (url) => (
-        <img src={(url = url == null ? "" : url)} style={{ width: "80px" ,height:"100px"}} />
+        <img src={url} style={{ width: "80px", height: "100px" }} />
+      ),
+    },
+    {
+      title: "Expertises",
+      key: "expertises",
+      dataIndex: "expertises",
+      render: (tags) => (
+        <span>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </span>
       ),
     },
     {
@@ -111,11 +149,7 @@ const Counselor = () => {
       dataIndex: "account",
       key: "account",
     },
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-    },
+
     {
       title: "是否通過認證",
       dataIndex: "isverify",
@@ -131,14 +165,16 @@ const Counselor = () => {
       return {
         id: u.ID,
         name: u.UserName.Name.LastName + u.UserName.Name.FirstName,
-        photo: u.Photo,
+        email: u.Email,
+        expertises: u.Expertises.map((r) => r.Skill),
+        photo: u.Photo == "" ? img_account : u.Photo,
         account: u.Email,
         isverify: u.IsVerify ? "Y" : "N",
       };
     });
     setUserData(form);
     setUserCount(result.length);
-  }
+  };
   useEffect(() => {
     fetchData();
   }, []);
@@ -154,7 +190,98 @@ const Counselor = () => {
     setRecord(null);
     setVisible(false);
   };
-
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleMenuClick = (e) => {
+    setSelectedCategory(e.key);
+  };
+  const items1 = ["1", "2", "3"].map((key) => ({
+    key,
+    label: `nav ${key}`,
+  }));
+  const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+    (icon, index) => {
+      const key = String(index + 1);
+      return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+      };
+    }
+  );
+  const items = [
+    {
+      key: '1',
+      label: 'Product',
+      children: 'Cloud Database',
+    },
+    {
+      key: '2',
+      label: 'Billing Mode',
+      children: 'Prepaid',
+    },
+    {
+      key: '3',
+      label: 'Automatic Renewal',
+      children: 'YES',
+    },
+    {
+      key: '4',
+      label: 'Order time',
+      children: '2018-04-24 18:00:00',
+    },
+    {
+      key: '5',
+      label: 'Usage Time',
+      span: 2,
+      children: '2019-04-24 18:00:00',
+    },
+    {
+      key: '6',
+      label: 'Status',
+      span: 3,
+      children: <Badge status="processing" text="Running" />,
+    },
+    {
+      key: '7',
+      label: 'Negotiated Amount',
+      children: '$80.00',
+    },
+    {
+      key: '8',
+      label: 'Discount',
+      children: '$20.00',
+    },
+    {
+      key: '9',
+      label: 'Official Receipts',
+      children: '$60.00',
+    },
+    {
+      key: '10',
+      label: 'Config Info',
+      children: (
+        <>
+          Data disk type: MongoDB
+          <br />
+          Database version: 3.4
+          <br />
+          Package: dds.mongo.mid
+          <br />
+          Storage space: 10 GB
+          <br />
+          Replication factor: 3
+          <br />
+          Region: East China 1
+          <br />
+        </>
+      ),
+    },
+  ];
   return (
     <>
       <Statistic
@@ -172,6 +299,48 @@ const Counselor = () => {
         onClose={handleClose}
         record={record}
       />
+      <Modal
+        title="Information"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        style={{ top: 20 }} // 设置高度为80%视窗高度
+        width={'80%'}
+        bodyStyle={{ height: "55vh" }}
+      >
+        <Layout>
+          <Layout>
+            <Sider width={200} style={{ background: colorBgContainer }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={["1"]}
+                defaultOpenKeys={["sub1"]}
+                style={{ height: "100%", borderRight: 0 }}
+                items={items2}
+              />
+            </Sider>
+            <Layout style={{ background: colorBgContainer, overflowY: 'auto'}}>
+        
+              <Content
+                style={{
+                  background: colorBgContainer,
+                  padding: 0,
+                  margin: 10,
+                  minHeight: 300,
+                  maxHeight:450
+                }}
+              >
+                <Descriptions 
+                extra={<Button type="primary">Edit</Button>}
+                bordered 
+                title="User Info" 
+                 items={items} />
+                
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
+      </Modal>
     </>
   );
 };
