ec571b8e575a1eaa7ebf93a913f207ac7b262af7
DESKTOP-UO9P3A3\User <likesm0887@gmail.com>
2023-10-04 00:46:02 +0800

    remove reqct query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Header from "./view/header/Header";
-import Body from "./view/body/Body";
-import { requestForToken } from "./service/firebase/firebase_fcm";
-import { onMessageListener } from "./service/firebase/firebase_receive_message";
 import { boot } from "./service/ServicePool";
-import { BrowserRouter, Outlet, HashRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Outlet,
+  HashRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Login from "./view/login/login";
 import Login2 from "./view/body/admin/Login";
 import Home from "./view/home/Home";
@@ -26,60 +28,58 @@ import User from "./view/body/admin/User";
 import Membership from "./view/body/admin/Membership";
 import Counselor from "./view/body/admin/Counselor";
 import Banner from "./view/body/admin/Banner";
-import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    QueryClient,
-    QueryClientProvider,
-  } from 'react-query'
-  
-const queryClient = new QueryClient()
-function App() {
-    // registerServiceWorker()
-    // Notification()
-    boot()
-    return (
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="Login" element={<Login2 />}></Route>
-                <Route path="admin" element={<Admin />}>
-                    <Route path="course" element={<Course />}></Route>
-                    <Route path="banner" element={<Banner />}></Route>
-                    <Route path="music" element={<Music />}></Route>
-                    <Route path="category" element={<Category />}></Route>
-                    <Route path="teacher" element={<Teacher />}></Route>
-                    <Route path="user" element={<User />}></Route>
-                    <Route path="setting" element={<Setting />}></Route>
-                    <Route path="membership" element={<Membership />}></Route>
-                    <Route path="counselor" element={<Counselor />}></Route>
-                </Route>
-                <Route path="couchspace-cms/register" element={<Register />} />
 
-                <Route path="couchspace-cms/" element={<Login />} />
-                <Route path="couchspace-cms/home" element={<Home />}>
-                    <Route>
-                        <Route index element={<Consultation />} />
-                        <Route>
-                            <Route path="consultation" element={<Consultation />} />
-                            <Route>
-                                <Route path="consultation/:id" element={<AppointmentDetail />} />
-                                <Route path="consultation/counseling/:id" element={<Counseling />} />
-                            </Route>
-                        </Route>
-                        <Route path="meditation" element={<Meditation />} />
-                        <Route path="repair" element={<Repair />} />
-                        <Route path="basicInfo" element={<BasicInfo />} />
-                        <Route path="counselingInfo" element={<CounselingInfo />} />
-                        <Route path="counselingManagement" element={<CounselingManagement />} />
-                    </Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
-       
-    </QueryClientProvider> 
-    );
+function App() {
+  // registerServiceWorker()
+  // Notification()
+  boot();
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="Login" element={<Login2 />}></Route>
+        <Route path="admin" element={<Admin />}>
+          <Route path="course" element={<Course />}></Route>
+          <Route path="banner" element={<Banner />}></Route>
+          <Route path="music" element={<Music />}></Route>
+          <Route path="category" element={<Category />}></Route>
+          <Route path="teacher" element={<Teacher />}></Route>
+          <Route path="user" element={<User />}></Route>
+          <Route path="setting" element={<Setting />}></Route>
+          <Route path="membership" element={<Membership />}></Route>
+          <Route path="counselor" element={<Counselor />}></Route>
+        </Route>
+        <Route path="couchspace-cms/register" element={<Register />} />
+
+        <Route path="couchspace-cms/" element={<Login />} />
+        <Route path="couchspace-cms/home" element={<Home />}>
+          <Route>
+            <Route index element={<Consultation />} />
+            <Route>
+              <Route path="consultation" element={<Consultation />} />
+              <Route>
+                <Route
+                  path="consultation/:id"
+                  element={<AppointmentDetail />}
+                />
+                <Route
+                  path="consultation/counseling/:id"
+                  element={<Counseling />}
+                />
+              </Route>
+            </Route>
+            <Route path="meditation" element={<Meditation />} />
+            <Route path="repair" element={<Repair />} />
+            <Route path="basicInfo" element={<BasicInfo />} />
+            <Route path="counselingInfo" element={<CounselingInfo />} />
+            <Route
+              path="counselingManagement"
+              element={<CounselingManagement />}
+            />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
