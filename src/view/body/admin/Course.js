c4a0b4fceb152de08947583bc57a869ed0bc9556
DESKTOP-UO9P3A3\User <likesm0887@gmail.com>
2023-10-26 00:29:00 +0800

    Adjust Course

--- src/view/body/admin/Course.js
+++ src/view/body/admin/Course.js
@@ -184,7 +184,7 @@ function Course() {
       }
 
       result.push({
-        key: res[i]._id,
+        key: res[i].CourseID,
         courseName: res[i].CourseName,
         image: res[i].Image,
         series: ["nice", "developer"],
@@ -303,7 +303,7 @@ function Course() {
 
   const createOptions = (musics) => {
     musics.forEach((m) => {
-      options.push({ value: m._id, label: m.Title });
+      options.push({ value: m.MusicID, label: m.Title });
     });
     setAllOption(options);
   };
