5496a5c69c80c3e2b66f53a898c205dd5d277e85
tyrion_kung <tyrion_kung@gmail.com>
2025-05-18 02:11:20 +0800

    adjust login/register

--- src/view/body/Body.js
+++ src/view/body/Body.js
@@ -4,7 +4,7 @@ import "./Body.css"
 const screenWidth = window.innerWidth;
 function Body() {
     return (
-        <div class="container-fluid p-0" style={{ height: "85%", width: "100%", backgroundColor: "#88a1d2" }}>
+        <div class="container-fluid p-0" style={{ height: "85%", width: "100%" }}>
             {screenWidth > 500 ?
                 <div class="row h-100">
                     {/* <div class="col-4 col-sm-2 col-lg-2 col-xxl-1 no-padding h-100">
